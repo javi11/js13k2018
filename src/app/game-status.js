9def6a2524332da2801ef6fa960c39e73026cf4f
javier.blanco@axa-groupsolutions.com <javicuba11@gmail.com>
2018-09-12 11:04:25 +0200

    fix: build

--- src/app/game-status.js
+++ src/app/game-status.js
@@ -4,19 +4,10 @@ export default class GameStatus {
     this.torch = torch;
     this.torchDecreaseRate = torchDecreaseRate;
     this.life = life;
-
-    const torchImg = document.getElementById('torchImg');
-    torchImg.src = require('../assets/sprites/fire.png');
-    this.torchVal = document.getElementById('torchValue');
-
-    const counterImg = document.getElementById('counterImg');
-    counterImg.src = require('../assets/sprites/clock.png');
-    this.counterVal = document.getElementById('counterVal');
-
-    const runImg = document.getElementById('runImg');
-    runImg.src = require('../assets/sprites/run.png');
-    this.runVal = document.getElementById('runVal');
-    this.runDiv = document.getElementById('run');
+    this.torchVal = document.getElementById('tv');
+    this.counterVal = document.getElementById('cv');
+    this.runVal = document.getElementById('rv');
+    this.runDiv = document.getElementById('r');
   }
 
   update(hero) {
--- src/index.js
+++ src/index.js
@@ -4,11 +4,10 @@ import Conversation from './app/conversation';
 import introScript from './assets/intro.json';
 import gameOverScript from './assets/game-over.json';
 import winScript from './assets/win.json';
-import winButNoScript from './assets/win-but-no.json';
 
-const display = document.querySelector('#counterVal');
+const display = document.querySelector('#cv');
 const restart = document.querySelector('#restart');
-const gameDiv = document.querySelector('#game');
+const gameDiv = document.querySelector('#g');
 const canvas = document.getElementsByTagName('canvas')[0];
 const conversationDiv = document.querySelector('.text');
 
@@ -32,19 +31,8 @@ function onGameOver() {
 function onWin(timer, duration) {
   gameDiv.style.display = 'none';
   const conversation = new Conversation(2000);
-  const showRestart = false;
   conversationDiv.style.display = 'block';
-  let script = winScript;
-  if (timer > duration - 25) {
-    script = winButNoScript;
-    showRestart = true;
-  }
-  conversation.startConversation(script, conversationDiv, () => {
-    if (showRestart) {
-      restart.style.display = 'block';
-      restart.onclick = startGame;
-    }
-  });
+  conversation.startConversation(winScript, conversationDiv, () => {});
 }
 
 conversationDiv.style.display = 'block';
